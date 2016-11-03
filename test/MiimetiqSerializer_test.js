15fdbe3d18f4696c2e3ff01f8504ec88ab35500f
Dinesh Manajipet <saidinesh5@gmail.com>
2016-11-04 03:49:08 +0530

    Created a MiimetiqSerializer
    Provides pack/unpack functions for dataType <-> messageType conversions.
    Currently supports: boolean, number, string data types.

---
+++ test/MiimetiqSerializer_test.js
@@ -0,0 +1,40 @@
+const MiimetiqSerializer = require('../lib/MiimetiqSerializer.js');
+const assert = require('assert');
+
+describe('MiimetiqSerializer' , function(){
+
+  let pack = MiimetiqSerializer.pack;
+  let unpack = MiimetiqSerializer.unpack;
+
+  describe('pack(boolean)', function(){
+    it('pack(boolean)', function(){
+      assert.deepEqual(pack(true, "boolean"), new Buffer('true'));
+      assert.deepEqual(pack(false, "boolean"), new Buffer('false'));
+    });
+  });
+
+  describe('unpack(pack(boolean))', function(){
+    it('unpack(pack(true, "boolean"), "boolean") = true', function(){
+      assert.equal(unpack(pack(true, "boolean"), "boolean"), true);
+    });
+    it('unpack(pack(false, "boolean"), "boolean") = false', function(){
+      assert.equal(unpack(pack(false, "boolean"), "boolean"), false);
+    });
+  });
+
+  describe('unpack(pack(number))', function(){
+    it('unpack(pack(5, "number"), "number") = 5', function(){
+      assert.equal(unpack(pack(5, "number"), "number"), 5);
+    });
+    it('unpack(pack(5.5, "number"), "number") = 5', function(){
+      assert.equal(unpack(pack(5.5, "number"), "number"), 5.5);
+    });
+  });
+
+  describe('unpack(pack(string))', function(){
+    it('unpack(pack("hello", "string"), "string") = "hello"', function(){
+      assert.equal(unpack(pack("hello", "string"), "string"), "hello");
+    });
+  });
+  
+});
